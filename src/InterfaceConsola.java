f1d76f48fe5fa5fe3c5654707679bbd8eed5e445
Daniel Herranz Chenlo <daniel2841995@gmail.com>
2024-05-19 17:30:23 +0200

    Finalizado con nivel fácil.

--- src/InterfaceConsola.java
+++ src/InterfaceConsola.java
@@ -1,6 +1,76 @@
+import motor3R.TresEnRaya;
+
+import java.util.Scanner;
 
 public class InterfaceConsola {
+
+    private TresEnRaya tablero;
+    private Scanner sc;
+    private int fila;
+    private int columna;
+    int dificultad = 1;
+
+    InterfaceConsola(){
+        tablero = new TresEnRaya();
+        sc = new Scanner(System.in);
+        presentarjuego();
+        turnoJugador();
+    }
+
+    private void mostrarTablero(){
+        System.out.println("Estado actual del tablero:");
+        String[][] tablero = this.tablero.getTablero();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(tablero[i][j]);
+                if (j == 0 || j == 1){
+                    System.out.print(" | ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private void presentarjuego(){
+        System.out.println("Bienvenido al juego de 3 en raya");
+        mostrarTablero();
+        System.out.println("Selecciona la dificultad del juego: ");
+        System.out.println("1. Fácil");
+        System.out.println("2. Intermedio");
+        System.out.println("3. Difícil");
+        dificultad = sc.nextInt();
+    }
+
+    private void turnoJugador(){
+        System.out.println("Turno del jugador...");
+        System.out.print("Introduce la fila: ");
+        fila = sc.nextInt();
+        System.out.print("Introduce la columna: ");
+        columna = sc.nextInt();
+        if (tablero.cambiarValorJugador(fila-1, columna-1)){
+            mostrarTablero();
+            System.out.println("Has ganado");
+            return;
+        } else {
+            mostrarTablero();
+            turnoMaquina();
+        }
+
+    }
+
+    private void turnoMaquina(){
+        System.out.println("Turno de la máquina...");
+        if (tablero.cambiarValorMaquina(fila, columna, dificultad)){
+            mostrarTablero();
+            System.out.println("La máquina ha ganado");
+            return;
+        } else {
+            mostrarTablero();
+            turnoJugador();
+        }
+    }
+
     public static void main(String[] args) {
-        
+        new InterfaceConsola();
     }
 }
--- src/motor3R/TresEnRaya.java
+++ src/motor3R/TresEnRaya.java
@@ -1,4 +1,100 @@
 package motor3R;
+
 public class TresEnRaya {
    
+    private String[][] tablero = new String[3][3];
+    private String valorCheck;
+    private boolean gana;
+
+    public TresEnRaya(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                tablero[i][j] = "_";
+            }
+        }
+        gana = false;
+    }
+
+    public String[][] getTablero(){
+        return tablero;
+    }
+
+    public boolean cambiarValorJugador(int filaJugador, int columnaJugador){
+        if (!tablero[filaJugador][columnaJugador].equals("_")){
+            return false;
+        } else {
+            tablero[filaJugador][columnaJugador] = "X";
+        }
+        if (alguienGano(filaJugador, columnaJugador)){
+            return true;
+        } else {return false;}
+    }
+
+    public boolean cambiarValorMaquina(int filaMaquina, int columnaMaquina, int dificultad){
+        if (dificultad == 1) {
+            filaMaquina = (int)(Math.random() * 3); // Generate random value within the valid range of the game board
+            columnaMaquina = (int)(Math.random() * 3); // Generate random value within the valid range of the game board
+            if (!tablero[filaMaquina][columnaMaquina].equals("_")){
+                cambiarValorMaquina(filaMaquina, columnaMaquina, dificultad); // Pass the correct arguments to the recursive call
+            } else {
+                tablero[filaMaquina][columnaMaquina] = "O";
+            }
+            if (alguienGano(filaMaquina, columnaMaquina)){
+                return true;
+            } else {return false;}
+        }
+        //TO-DO: Implementar la lógica para los niveles de dificultad 2 y 3.
+        else {
+            return false;
+        }
+    }
+
+    public boolean alguienGano(int fila, int columna){
+        valorCheck = tablero[fila][columna];
+        // Comprobar si ha ganado en la fila
+        for (int i = 0; i < 3; i++){
+            if (!tablero[fila][i].equals(valorCheck)){
+                break;
+            }
+            if (i == 2){
+                gana = true;
+            }
+        }
+        // Comprobar si ha ganado en la columna
+        for (int i = 0; i < 3; i++){
+            if (!tablero[i][columna].equals(valorCheck)){
+                break;
+            }
+            if (i == 2){
+                gana = true;
+            }
+        }
+        // Comprobar si ha ganado en la diagonal
+        if (fila == columna){
+            for (int i = 0; i < 3; i++){
+                if (!tablero[i][i].equals(valorCheck)){
+                    break;
+                }
+                if (i == 2){
+                    gana = true;
+                }
+            }
+        }
+        // Comprobar si ha ganado en la diagonal inversa
+        if (fila + columna == 2){
+            for (int i = 0; i < 3; i++){
+                if (!tablero[i][2-i].equals(valorCheck)){
+                    break;
+                }
+                if (i == 2){
+                    gana = true;
+                }
+            }
+        }
+        if (gana){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
